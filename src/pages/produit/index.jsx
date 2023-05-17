f7f53df0a0dc564f01501239395915cde3a8f615
francois lalay <francois.lalay@gmail.com>
2023-05-17 14:27:59 +0200

    add filter status on product page

--- src/pages/produit/index.jsx
+++ src/pages/produit/index.jsx
@@ -1,17 +1,48 @@
 import ListData from "@/components/general/ListData.jsx";
 import Link from "next/link.js";
+import { useState } from "react";
 
-export default function produit({products}) {
-  console.log(products)
+export default function produit({ products }) {
+  const [filter, setFilter] = useState("deal");
+
+  const listProducts = [];
+
+  products.forEach((product) => {
+    if (filter === "all") {
+      listProducts.push(product);
+    } else if (filter === "deal" && product.isDeal) {
+      listProducts.push(product);
+    } else if (filter === "archive" && product.isArchive) {
+      listProducts.push(product);
+    } else if (
+      filter === "sale" &&
+      product.isDeal === false &&
+      product.isArchive === false
+    ) {
+      listProducts.push(product);
+    }
+  });
+
   return (
     <>
       <main>
         <section>
           <div className="flex justify-between">
             <h2 className="font-bold text-3xl">Gestion des produits</h2>
-            <Link href='/produit/ajouter' className="bg-green-600 text-white text-sm rounded self-center px-3 py-1">Ajouter</Link>
+            <Link
+              href="/produit/ajouter"
+              className="bg-green-600 text-white text-sm rounded self-center px-3 py-1"
+            >
+              Ajouter
+            </Link>
           </div>
-          <ListData products={products}/>
+          <div>
+            <button onClick={() => setFilter("all")}>Tous</button>
+            <button onClick={() => setFilter("sale")}>En vente</button>
+            <button onClick={() => setFilter("deal")}>Promo</button>
+            <button onClick={() => setFilter("archive")}>Archive</button>
+          </div>
+          <ListData products={listProducts} />
         </section>
       </main>
     </>
@@ -19,14 +50,11 @@ export default function produit({products}) {
 }
 
 export async function getServerSideProps() {
-
   const res = await fetch(process.env.NEXT_PUBLIC_URL_API + "/product");
-  const products =  await res.json();
-   return {
+  const products = await res.json();
+  return {
     props: {
-      products
-    }
-   }
+      products,
+    },
+  };
 }
-
-
