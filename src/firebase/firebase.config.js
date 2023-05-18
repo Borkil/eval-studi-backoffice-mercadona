a481ca22a920d60c65e293d3bd391e2e7e2331cf
francois lalay <francois.lalay@gmail.com>
2023-05-18 11:27:11 +0200

    secret config firebase

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 export  const firebaseConfig = {
-  apiKey: "AIzaSyAB7TgajBJY3smLhgd-K4JqVZpnlJzSjFI",
-  authDomain: "mercadona-89b8e.firebaseapp.com",
-  projectId: "mercadona-89b8e",
-  storageBucket: "mercadona-89b8e.appspot.com",
-  messagingSenderId: "397740657145",
-  appId: "1:397740657145:web:80b5e5c37cc5204a061528"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STRORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
